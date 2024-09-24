f3e7f9efaef29e0861aa7363ee4930ac5a22cbcc
MRamirez202210582 <U202210582@upc.edu.pe>
2024-09-23 22:58:56 -0500

    fix and uploaf file home activity

--- app/src/main/java/com/example/gastrotrack_appmovil/HomeActivity.kt
+++ app/src/main/java/com/example/gastrotrack_appmovil/HomeActivity.kt
@@ -1,4 +1,49 @@
 package com.example.gastrotrack_appmovil
 
-class HomeActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+
+        val ibInventory = findViewById<ImageButton>(R.id.ibInventory)
+        val ibNotificaction = findViewById<ImageButton>(R.id.ibNotificaction)
+        val ibTeam = findViewById<ImageButton>(R.id.ibTeam)
+        val ibProfile = findViewById<ImageView>(R.id.ivProfile)
+        val ibExit = findViewById<ImageButton>(R.id.ibExit)
+
+        ibInventory.setOnClickListener {
+            val intent =
+                Intent(this, InventoryActivity::class.java)
+            startActivity(intent)
+        }
+
+        ibNotificaction.setOnClickListener {
+            val intent =
+                Intent(this, NotificationActivity::class.java)
+            startActivity(intent)
+        }
+
+        ibTeam.setOnClickListener {
+            val intent = Intent(this, TeamActivity::class.java) /
+            startActivity(intent)
+        }
+
+        ibProfile.setOnClickListener {
+            val intent =
+                Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        ibExit.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() 
+        }
+    }
 }
