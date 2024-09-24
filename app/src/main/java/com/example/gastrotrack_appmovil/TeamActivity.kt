5c683a723fb290f3644af195f315ba89e3fe3978
MRamirez202210582 <U202210582@upc.edu.pe>
2024-09-23 23:27:16 -0500

    add routes team

--- app/src/main/java/com/example/gastrotrack_appmovil/TeamActivity.kt
+++ app/src/main/java/com/example/gastrotrack_appmovil/TeamActivity.kt
@@ -1,4 +1,11 @@
 package com.example.gastrotrack_appmovil
 
-class TeamActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TeamActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.team)
+    }
 }
