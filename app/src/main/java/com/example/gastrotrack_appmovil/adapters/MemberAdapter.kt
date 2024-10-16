efaab0ec439656b2fc5719761595d83a9103c68d
Alejandro Zárate <u20181h198@upc.edu.pe>
2024-10-15 20:05:48 -0500

    feat: Implement Members feature with API, DAO, and Adapter

--- app/src/main/java/com/example/gastrotrack_appmovil/adapters/MemberAdapter.kt
+++ app/src/main/java/com/example/gastrotrack_appmovil/adapters/MemberAdapter.kt
@@ -1,4 +1,37 @@
 package com.example.gastrotrack_appmovil.adapters
 
-class MemberAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gastrotrack_appmovil.R
+import com.example.gastrotrack_appmovil.models.Members
+
+class MemberAdapter(private val membersList: List<Members>) : RecyclerView.Adapter<MemberAdapter.MemberViewHolder>() {
+
+    class MemberViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val ivMemberPhoto: ImageView = itemView.findViewById(R.id.ivMemberPhoto)
+        val tvMemberName: TextView = itemView.findViewById(R.id.tvMemberName)
+        val tvDescriptionMember: TextView = itemView.findViewById(R.id.tvDescriptionMember)
+        val tvRoleMember: TextView = itemView.findViewById(R.id.tvRoleMember)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemberViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.prototype_members, parent, false)
+        return MemberViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MemberViewHolder, position: Int) {
+        val member = membersList[position]
+        holder.tvMemberName.text = member.name
+        //holder.tvDescriptionMember.text = member.description
+        //holder.tvRoleMember.text = member.role
+    }
+
+    override fun getItemCount(): Int {
+        return membersList.size
+    }
 }
--- app/src/main/java/com/example/gastrotrack_appmovil/network/MemberService.kt
+++ app/src/main/java/com/example/gastrotrack_appmovil/network/MemberService.kt
@@ -1,4 +1,11 @@
 package com.example.gastrotrack_appmovil.network
 
+import com.example.gastrotrack_appmovil.communication.MembersApiResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface MemberService {
+    @POST("api/v1/membrers")
+    fun createMember(@Body member: MembersApiResponse): Call<MembersApiResponse>
 }
