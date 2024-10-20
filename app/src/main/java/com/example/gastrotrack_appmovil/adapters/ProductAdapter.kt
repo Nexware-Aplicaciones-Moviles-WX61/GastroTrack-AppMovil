bf02aeab99225bde5cb313c03fe0a2d4a6949bb8
JairCoraje <102072560+JairCoraje@users.noreply.github.com>
2024-10-20 15:58:53 -0500

    feat: implement adapter from product

--- app/src/main/java/com/example/gastrotrack_appmovil/adapters/ProductAdapter.kt
+++ app/src/main/java/com/example/gastrotrack_appmovil/adapters/ProductAdapter.kt
@@ -1,4 +1,69 @@
 package com.example.gastrotrack_appmovil.adapters
 
-class ProductAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.gastrotrack_appmovil.R
+import com.example.gastrotrack_appmovil.models.Product
+import com.squareup.picasso.Picasso
+
+class ProductAdapter(private val products: List<Product>, val clickLister: OnItemClickListener) : Adapter<ProductAdapter.ProductViewHolder>() {
+
+    inner class ProductViewHolder(itemView: View): ViewHolder(itemView) {
+        private val tvProductName = itemView.findViewById<TextView>(R.id.tvProductName)
+        private val tvDateManufacture = itemView.findViewById<TextView>(R.id.tvDateManufacture)
+        private val tvExperitation = itemView.findViewById<TextView>(R.id.tvExperitation)
+        private val tvStock = itemView.findViewById<TextView>(R.id.tvStock)
+        private val tvState = itemView.findViewById<TextView>(R.id.tvState)
+        private val ivProductPhoto = itemView.findViewById<ImageView>(R.id.ivProductPhoto)
+
+        private val btDeleteProduct = itemView.findViewById<ImageButton>(R.id.btnDeleteProduct)
+        private val btnEditProduct = itemView.findViewById<ImageButton>(R.id.btnEditProduct)
+
+        fun bind(products: Product, clicListener: OnItemClickListener){
+            tvProductName.text = products.name
+            tvDateManufacture.text = products.dateManufacture
+            tvExperitation.text = products.dueDate
+            tvStock.text = products.stock.toString()
+            tvState.text = products.state
+            Picasso.get()
+                .load(products.image)
+                .into(ivProductPhoto)
+
+            btDeleteProduct.setOnClickListener {
+                clickLister.onItemClick(products)
+            }
+
+            btnEditProduct.setOnClickListener {
+                clickLister.onItemClick(products)
+            }
+        }
+
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.prototype_product, parent, false)
+        return ProductViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return products.size
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        holder.bind(products[position], clickLister)
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(products: Product)
+    }
 }
+
+
--- app/src/main/java/com/example/gastrotrack_appmovil/adapters/ProductExpiredAdapter.kt
+++ app/src/main/java/com/example/gastrotrack_appmovil/adapters/ProductExpiredAdapter.kt
@@ -1,4 +1,49 @@
 package com.example.gastrotrack_appmovil.adapters
 
-class ProductExpiredAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.gastrotrack_appmovil.R
+import com.example.gastrotrack_appmovil.models.Product
+import com.squareup.picasso.Picasso
+
+class ProductExpiredAdapter(private val products: List<Product>) :
+    Adapter<ProductExpiredAdapter.ProductExpiredViewHolder>() {
+
+    inner class ProductExpiredViewHolder(itemView: View): ViewHolder(itemView) {
+        private val tvExpiredProductName = itemView.findViewById<TextView>(R.id.tvExpiredProductName)
+        private val tvExpiredProduct = itemView.findViewById<TextView>(R.id.tvExpiredProduct)
+        private val ivExpiredProductPhoto = itemView.findViewById<ImageView>(R.id.ivExpiredProductPhoto)
+
+        fun bind(products: Product){
+            tvExpiredProductName.text = products.name
+            tvExpiredProduct.text = products.dueDate
+            Picasso.get()
+                .load(products.image)
+                .into(ivExpiredProductPhoto)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductExpiredViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.prototype_product_expired, parent, false)
+        return ProductExpiredViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return products.size
+    }
+
+    override fun onBindViewHolder(holder: ProductExpiredViewHolder, position: Int) {
+        holder.bind(products[position])
+    }
+
+
+
 }
+
+
